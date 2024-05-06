f502a78511f54bcb24f8b4a77627d6b3cab324c2
Letícia Rogére <143463078+LettyRogere@users.noreply.github.com>
2024-05-06 13:53:36 -0300

    Atualizar o config.php

--- sistema escolar/php/config.php
+++ sistema escolar/php/config.php
@@ -2,9 +2,9 @@
 //Configuração da conexão com o BD
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
-$dbHost = "212.1.208.201";
-$dbUserName = "u179408266_rogereLeticia";
-$dbPassword = "!8bCBocYb";
+$dbHost = "";
+$dbUserName "";
+$dbPassword = "";
 $dbName = "u179408266_acstem_escolas";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
